9c1a6e71c745af7cec159d0739e3452f5f11f643
Anh Tuan <tamtr1998@gmail.com>
2020-06-27 01:14:30 +0700

    them dang nhap ke toan

--- mavenproject1/src/main/java/view/GDQuanLiKho.java
+++ mavenproject1/src/main/java/view/GDQuanLiKho.java
@@ -80,6 +80,11 @@ public class GDQuanLiKho extends javax.swing.JFrame {
         });
 
         jButton1.setText("Đăng Xuất");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -256,6 +261,11 @@ public class GDQuanLiKho extends javax.swing.JFrame {
         dXuatHangFrm.setLocationRelativeTo(null);
         dXuatHangFrm.setVisible(true);
     }//GEN-LAST:event_jButtonXuatHangActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
